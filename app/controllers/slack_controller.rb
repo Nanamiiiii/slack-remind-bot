abf8fc27832726ea4a7d8cf72919ab5ecd80f0ee
Nanamiiiii <akihiro_0605@outlook.com>
2021-01-24 15:11:53 +0900

    create receive event

---
+++ app/controllers/slack_controller.rb
@@ -0,0 +1,13 @@
+class SlackController < ApplicationController
+  def index
+  end
+
+  def create
+    @body = JSON.parse(request.body.read)
+    case @body['type']
+    when 'url_verification'
+      render json: @body
+    when 'event_callback'
+      # something
+    end
+end
