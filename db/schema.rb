9a9f49fc0b8815c9c31ea917f91b1e0e9714f436
Nanamiiiii <akihiro_0605@outlook.com>
2021-01-27 23:46:23 +0900

    regenerate model

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_26_083040) do
+ActiveRecord::Schema.define(version: 2021_01_27_143808) do
 
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "slack_id"
-    t.boolean "validate"
+    t.boolean "valid_user"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
