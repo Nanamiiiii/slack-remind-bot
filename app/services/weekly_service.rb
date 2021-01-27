8a5a367fa42eb2ed76210495a6e7e5759a6a740a
Nanamiiiii <akihiro_0605@outlook.com>
2021-01-28 01:26:41 +0900

    fix some method call

--- app/services/weekly_service.rb
+++ app/services/weekly_service.rb
@@ -7,13 +7,13 @@ class WeeklyService
   end
 
   def reminder(today)
-    logger.info(today)
+    puts today
     day_of_week = today.wday
     now_hour = today.hour
     now_minute = today.min
 
     @weekly_model.find_each do |model|
-      logger.info(model.remind_time)
+      puts model.remind_time
       remind_day = model.day
       sc_h = model.remind_time.hour
       sc_m = model.remind_time.min
